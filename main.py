b7096685e218348e9c6a734d22d0e4353ed58250
ashyyaq <144699375+ashyyaq@users.noreply.github.com>
2023-09-21 11:01:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-# используются функции, которые могут выполнить повторяющиеся вычисления
-def add(num1, num2):
-    return num1 + num2
+def sum_digits(number):
+    number_str = str(number)
+    digit_sum = 0
+    for digit in number_str:
+        digit_sum += int(digit)
+    return digit_sum
 
-def subtract(num1, num2):
-    return num1 - num2
-
-def multiply(num1, num2):
-    return num1 * num2
-
-def divide(num1, num2):
-    if num2 == 0:
-        return "Ошибка. Деление на ноль"
-    return num1 / num2
-
-# Основной цикл калькулятора
-while True:
-    operation = input("Введите операцию (+, -, *, / или x для завершения): ")
-    if operation == "x":
-        break
-
-    number1 = float(input("Введите первое число: "))
-    number2 = float(input("Введите второе число: "))
-
-    if operation == "+":
-        result = add(number1, number2)
-    elif operation == "-":
-        result = subtract(number1, number2)
-    elif operation == "*":
-        result = multiply(number1, number2)
-    elif operation == "/":
-        result = divide(number1, number2)
-
-    else:
-        print("Ошибка: неизвестная операция")
-        continue
-
-    print("Результат:", result)
-
+num = int(input("Введите положительное целое число: "))
+result = sum_digits(num)
+print(result)
